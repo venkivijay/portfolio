0eac77b9aa845ce1d9b7efb2eba5c7e32260f541
venkivijay <venkivijay@hotmail.com>
2021-08-08 11:17:28 +0530

    Feat: Mobile Menu

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
 	<header
-		class="sticky top-0 z-30 flex items-center justify-between py-4 bg-opacity-50 border-b  c-overall-p backdrop-filter backdrop-blur border-secondary"
+		class="sticky top-0 z-50 flex items-center justify-between py-4 bg-opacity-50 border-b c-overall-p backdrop-filter backdrop-blur border-secondary"
 	>
 		<p class="text-4xl leading-none font-brand text-accent">VV</p>
 		<nav class="hidden md:block">
@@ -16,14 +16,36 @@
 				</li>
 			</ul>
 		</nav>
-		<div class="ham">
-			<Icon primaryIcon="menu" class="w-10 h-10" />
-		</div>
+		<!-- Hamburger Menu Button -->
+		<button
+			class="absolute pt-5 cursor-pointer top-4 right-4 focus:outline-none"
+			:class="isMenuOpen ? 'is-open' : ''"
+			@click="isMenuOpen = !isMenuOpen"
+		>
+			<span class="relative block h-1 mx-0 my-auto skew-y-6 w-7 bg-accent burger"></span>
+		</button>
 	</header>
+	<TheMobileMenu :isMenuOpen="isMenuOpen" />
 </template>
 
 <script setup>
 	import Icon from "components/Icon.vue"
+	import TheMobileMenu from "components/TheMobileMenu.vue"
+</script>
+
+<script>
+	export default {
+		data() {
+			return {
+				isMenuOpen: false,
+			}
+		},
+		watch: {
+			isMenuOpen() {
+				document.body.style.overflow = this.isMenuOpen ? "hidden" : "auto"
+			},
+		},
+	}
 </script>
 
 <style scoped>
@@ -32,5 +54,34 @@
 	}
 	ul > * {
 		@apply flex-1 px-4;
+	}
+	.burger {
+		transform: skew(5deg);
+		transition: all 0.275s;
+	}
+	.burger:after,
+	.burger:before {
+		content: "";
+		transition: all 0.275s;
+		@apply h-full block bg-accent;
+	}
+	.burger:after {
+		transform: translateY(-12px) translateX(-2px) skew(-20deg);
+	}
+
+	.burger:before {
+		transform: translateY(-16px) skew(-10deg);
+	}
+	.is-open .burger {
+		transform: translateY(-8px) rotate(-45deg);
+	}
+
+	.is-open .burger:before {
+		transform: translateY(0px) skew(-10deg) rotate(75deg);
+	}
+
+	.is-open .burger:after {
+		transform: translateY(-12px) translateX(10px) skew(-20deg);
+		opacity: 0;
 	}
 </style>
---
+++ src/components/TheMobileMenu.vue
@@ -0,0 +1,91 @@
+<template>
+	<nav
+		class="fixed top-0 bottom-0 left-0 right-0 invisible overflow-hidden text-center opacity-0 bg-accent-alpha"
+		:class="isMenuOpen ? 'is-open' : ''"
+	>
+		<ul class="inline-flex flex-col items-end justify-center h-full">
+			<li class="c-active"><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Skills</a></li>
+			<li><a href="#">Portfolio</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+	export default {
+		props: {
+			isMenuOpen: {
+				required: true,
+				type: Boolean,
+			},
+		},
+	}
+</script>
+
+<style scoped>
+	nav {
+		z-index: -1;
+		transition: all 0.275s 0.1s;
+	}
+	nav.is-open {
+		@apply opacity-100 z-40 visible;
+	}
+
+	nav::before {
+		content: "";
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: -15px;
+		transform-origin: 0 0;
+		transform: skew(-14deg) translateX(-120%);
+		transition: all 0.275s 0.1s;
+		@apply bg-background;
+	}
+
+	nav.is-open::before {
+		transform: skew(-14deg) translateX(0);
+	}
+
+	nav ul,
+	nav li {
+		@apply list-none p-0;
+	}
+
+	nav ul {
+		transform: translateX(-18%) skew(-16deg);
+	}
+
+	nav li {
+		transform: skew(16deg);
+		@apply block my-2 mx-0 text-right;
+	}
+
+	nav a {
+		@apply opacity-0 -translate-y-3 block px-3 py-0 text-4xl font-bold text-primary;
+		text-decoration: none;
+	}
+
+	nav.is-open a {
+		@apply opacity-100 translate-y-0;
+	}
+
+	nav li:nth-child(1) a {
+		transition: all 275ms 175ms;
+	}
+	nav li:nth-child(2) a {
+		transition: all 275ms 225ms;
+	}
+	nav li:nth-child(3) a {
+		transition: all 275ms 275ms;
+	}
+	nav li:nth-child(4) a {
+		transition: all 275ms 325ms;
+	}
+	nav li:nth-child(5) a {
+		transition: all 275ms 375ms;
+	}
+</style>
