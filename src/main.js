7dc32969fa5bfdcd1d1033f9dbaf73731160aa2a
venkivijay <venkivijay@hotmail.com>
2021-10-29 20:51:14 +0530

    fix: ContactForm netlify integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
 import "./assets/css/index.css"
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).use(Toast).mount("#app")
