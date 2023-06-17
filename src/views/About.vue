beecc169880c8a852cdf0d7e69f9b716a005092c
venkivijay <venkivijay@hotmail.com>
2023-06-17 15:30:37 +0530

    Add new projects, update skills & info

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,10 @@
 			<router-link class="header-anchor" :to="{ hash: '#about' }">About</router-link>
 		</h2>
 		<p class="py-2">
-			My name is Venkatesh, also known online as Venki Vijay. I specialize in building quality web applications
-			that solve business problems, provide new insights and beautiful user experiences. I’m a massive fan of the
-			Jamstack and I have a wealth of knowledge working with VueJS. I like learning and experimenting with new
-			technologies as they come. Life is a continuous learning process, so work makes me more rigorous.
+			Highly skilled Senior DevOps Engineer with expertise in AWS cloud infrastructure. Proficient in configuring
+			and maintaining AWS services, implementing CI/CD pipelines, and optimizing cloud environments. Strong
+			knowledge of IaC, security practices, and troubleshooting. Effective collaborator and continuously expanding
+			skill set. Valuable asset for maximizing AWS capabilities in any organization.
 		</p>
 	</section>
 </template>
